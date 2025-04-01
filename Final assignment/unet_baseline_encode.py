c4178f62a1a84a75d30fc5e2edff74ed1c45d82b
RalphDubbeldam <ralphdubbeldam@gmail.com>
2025-04-01 21:23:40 +0200

    add

--- Final assignment/unet_baseline_encode.py
+++ Final assignment/unet_baseline_encode.py
@@ -1,43 +1,6 @@
 import torch
 import torch.nn as nn
 
-class Model(nn.Module):
-    """ 
-    A simple U-Net architecture for image segmentation.
-    Based on the U-Net architecture from the original paper:
-    Olaf Ronneberger et al. (2015), "U-Net: Convolutional Networks for Biomedical Image Segmentation"
-    https://arxiv.org/pdf/1505.04597.pdf
-    """
-    def __init__(self, in_channels=3, n_classes=16):
-        
-        super(Model, self).__init__()
-
-        self.inc = (DoubleConv(in_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
-        self.down4 = (Down(512, 512))
-        self.up1 = (Up(1024, 256))
-        self.up2 = (Up(512, 128))
-        self.up3 = (Up(256, 64))
-        self.up4 = (Up(128, 64))
-        self.outc = (OutConv(64, n_classes))
-
-    def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
-
-        return logits
-        
-
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
@@ -71,21 +34,6 @@ class Down(nn.Module):
     def forward(self, x):
         return self.maxpool_conv(x)
 
-
-class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
-
-
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(OutConv, self).__init__()
@@ -93,3 +41,77 @@ class OutConv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
+
+
+class AttentionBlock(nn.Module):
+    """Attention Gate to refine skip connections."""
+    def __init__(self, in_channels, gating_channels, inter_channels):
+        super(AttentionBlock, self).__init__()
+        
+        self.W_x = nn.Sequential(
+            nn.Conv2d(in_channels, inter_channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(inter_channels)
+        )
+        
+        self.W_g = nn.Sequential(
+            nn.Conv2d(gating_channels, inter_channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(inter_channels)
+        )
+        
+        self.psi = nn.Sequential(
+            nn.Conv2d(inter_channels, 1, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+        
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x, g):
+        x1 = self.W_x(x)
+        g1 = self.W_g(g)
+        psi = self.relu(x1 + g1)
+        psi = self.psi(psi)
+        return x * psi
+
+class Up(nn.Module):
+    """Upscaling with attention and then double conv"""
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.attention = AttentionBlock(in_channels // 2, in_channels // 2, in_channels // 4)
+        self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x2 = self.attention(x2, x1)
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class Model(nn.Module):
+    """U-Net with Attention Mechanism."""
+    def __init__(self, in_channels=3, n_classes=16):
+        super(Model, self).__init__()
+
+        self.inc = DoubleConv(in_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 512)
+        self.up1 = Up(1024, 256)
+        self.up2 = Up(512, 128)
+        self.up3 = Up(256, 64)
+        self.up4 = Up(128, 64)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
