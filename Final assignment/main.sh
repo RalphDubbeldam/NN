5f41c475616fd5fcd67d5c5122c30e01ec58e009
RalphDubbeldam <ralphdubbeldam@gmail.com>
2025-04-02 11:40:58 +0200

    add

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,7 +2,7 @@ wandb login
 
 python3 train_baseline_encode.py \
     --data-dir ./data/cityscapes \
-    --batch-size 16 \
+    --batch-size 4 \
     --epochs 100 \
     --lr 0.001 \
     --num-workers 10 \
