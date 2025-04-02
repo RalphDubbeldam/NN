b55aedb4bcdd4074cd7c2edb523a011302fba819
RalphDubbeldam <ralphdubbeldam@gmail.com>
2025-04-02 15:15:13 +0200

    add

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train_baseline.py \
+python3 train_baseline_encode.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 100 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "Baseline" \
+    --experiment-id "newtry" \
