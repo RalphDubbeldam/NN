ddcec964be3530dde943817cb470857467653a70
RalphDubbeldam <ralphdubbeldam@gmail.com>
2025-04-03 09:43:38 +0200

    add

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,7 +4,7 @@ python3 train_baseline_encode.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 100 \
-    --lr 0.001 \
+    --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "Bottleneck" \
+    --experiment-id "ResNet_0.0001" \
