3049a5644615ab87df72d2786e8b3b46b0482fa6
RalphDubbeldam <ralphdubbeldam@gmail.com>
2025-04-04 11:19:53 +0200

    add

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train_baseline_3D.py \
+python3 train_combined.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 100 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "3D" \
+    --experiment-id "Combined" \
